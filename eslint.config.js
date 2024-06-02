d4130408103fe4db98d5abf2153c1e6688b1c97c
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-06-02 00:16:07 -0700

    bug: update eslint config to remove stylistic plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,11 @@
-import stylistic from "@stylistic/eslint-plugin";
 import Lint from "@jimbojet/lint";
 
 export default [
   ...new Lint(
-    stylistic,
     {
       js: ["*.config.js"],
       ts: ["src/**/*.ts"],
     },
-    { },
   )
     .configs,
 ];
